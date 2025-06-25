5261891b6f1afc1750bf1a66b819cbf92d686317
Joshua Park <joshuapark1309@gmail.com>
2025-06-24 20:51:31 -0400

    Foundation Complete

--- src/components/Scripts.tsx
+++ src/components/Scripts.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useRef } from 'react'
 import * as THREE from 'three'
 import * as YUKA from "yuka"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
 
 
 export const Scripts  = () => {
@@ -10,19 +13,23 @@ export const Scripts  = () => {
     const width = window.innerWidth;
     const height = window.innerHeight;
 
-    // Renderer
+    // Renderer - JS API for rendering interactive 3D Graphics on web browser
     const renderer = new THREE.WebGLRenderer({ antialias: true})
     renderer.setSize(width, height);
     renderer.setClearColor(0xa3a3a3);
     mountRef.current?.appendChild(renderer.domElement);
 
-    // Scene
+    // Scene - Setting up what to render and where
     const scene = new THREE.Scene();
 
-    // Camera
+    // Camera (fov, aspect ratio, near, far) - fear/far is zooming
     const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
     camera.position.set(6, 10, 14);
     camera.lookAt(scene.position);
+
+    // Ability to rotate the camera
+    const controls = new OrbitControls(camera, renderer.domElement);
+
 
     // Lighting
     const ambientLight = new THREE.DirectionalLight(0xffffff, 1)
@@ -38,7 +45,7 @@ export const Scripts  = () => {
     vehicleGeometry.translate(0, 0.25, 0)
     const vehicleMaterial = new THREE.MeshNormalMaterial();
     const vehicleMesh = new THREE.Mesh(vehicleGeometry, vehicleMaterial);
-    vehicleMesh.matrixAutoUpdate = false;
+    vehicleMesh.matrixAutoUpdate = false; // Let YUKA handle the movements
     scene.add(vehicleMesh);
 
     // Adding YUKA Vehicle - Connecting body (mesh) and mind (YUKA)
@@ -52,6 +59,15 @@ export const Scripts  = () => {
     // Function necessary to keep track of entities
     const entityManager = new YUKA.EntityManager()
     entityManager.add(vehicle);
+
+
+    // Adding our factory floor
+    const loader = new GLTFLoader()
+    loader.load("/Original_Plane.glb", function (glb) {
+      const model = glb.scene;
+      scene.add(model)
+    })
+
 
     const time = new YUKA.Time();
 
